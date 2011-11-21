4c02c08c8512cac60753451b51ca2239ae7512db
Sergio Raposo Vargas <serrapos@gmail.com>
2011-11-21 18:28:09 +0100

    Subida pruebas con ManagedBen y Backing Bean

--- alquisport-web/src/main/java/es/tresw/service/RegisterService.java
+++ alquisport-web/src/main/java/es/tresw/service/RegisterService.java
@@ -2,7 +2,24 @@ package es.tresw.service;
 
 import org.springframework.stereotype.Service;
 
+import es.tresw.db.beans.Client;
+
 @Service
 public class RegisterService {
+	
+	private String mensajeok;
+	
+	public RegisterService()
+	{
+		mensajeok = "ha entrado y todo es correcto";
+	}
+	
+	 public boolean register(Client c)
+	 {
+		 //TODO registrar en BBDD el cliente
+		 System.out.println(mensajeok);
+		 
+		 return true;
+	 }
 
 }
--- alquisport-web/src/main/java/es/tresw/view/controller/RegisterController.java
+++ alquisport-web/src/main/java/es/tresw/view/controller/RegisterController.java
@@ -1,19 +1,54 @@
 package es.tresw.view.controller;
 
-import javax.inject.Named;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import es.tresw.db.beans.Client;
+import es.tresw.service.RegisterService;
 
-@Named("register")
+@ManagedBean
+@RequestScoped
 public class RegisterController {
 	
 	private Client client = new Client();
 	
 	/*SERVICE A UTILIZAR*/
+	@Autowired
+	private RegisterService registerService;
 	
-	public void registerFinalUser()
+	
+	/*METODOS ASOCIADOS A ACCIONES DE FORMULARIO*/
+	public void registerClient()
 	{
-		System.out.println("HA ENTRADO Y REGISTRADO AL USUARIO");
+		if(registerService!=null)
+		{
+			//Llamamos al service de registro para que guarde el cliente
+			registerService.register(client);
+			System.out.println("HA ENTRADO Y REGISTRADO AL USUARIO");
+		}
+		else
+			System.out.println("el service no llega!");
+	}
+
+	/*GETTERS AND SETTERS*/
+	public Client getClient() {
+		System.out.println("en el get");
+		client.setLogin("ESTE SOY YO");
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public RegisterService getRegisterService() {
+		return registerService;
+	}
+
+	public void setRegisterService(RegisterService registerService) {
+		this.registerService = registerService;
 	}
 
 }
